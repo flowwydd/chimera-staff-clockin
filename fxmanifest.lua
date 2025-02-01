c31a8e016e2e1bdc5f5d2e7312e523fd642a17ea
Flow <168638103+flowwydd@users.noreply.github.com>
2025-01-31 20:57:06 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,25 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'Danboi - Edited By Chimera Development'
+author 'Chimera Development'
 description 'Staff Clockin/Clockout'
-version '1.0.0'
+version '1.5.0'
 lua54 'yes'
 
 dependency 'ox_lib'
 
 shared_script 'config.lua'
 
-client_scripts {
+shared_scripts {
+  "config.lua",
   "@ox_lib/init.lua",
-  "client.lua",
   "functions.lua"
 }
 
+client_scripts {
+	'client.lua'
+}
+
 server_scripts {
-  "@ox_lib/init.lua",
-  "server.lua",
-  "functions.lua"
+	'server.lua'
 }
